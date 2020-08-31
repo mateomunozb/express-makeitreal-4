5cad818329f6cd850dc2e85337fa39193f0af2d4
root <root@DESKTOP-4I5C2KQ.localdomain>
2020-08-31 12:50:39 -0500

    Reto 4 express

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/makers/:nombre', (req, res) => {
+  const nombre = req.params.nombre[0].toUpperCase() + req.params.nombre.slice(1)
+  res.send(`<h1>Hola ${nombre}</h1>`)
+})
+app.listen(3000, () => console.log('Listening on port 3000!'))
